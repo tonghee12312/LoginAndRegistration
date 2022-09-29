c6cdc3742e6fee34d3a9cd7d6973bbee56f22f46
Forge-15 <Forge-15@DESKTOP-USIT37L>
2022-09-29 22:37:30 +0800

    added login and signup functionalitis

--- app/src/main/java/com/example/heechintong/LoginActivity.kt
+++ app/src/main/java/com/example/heechintong/LoginActivity.kt
@@ -4,17 +4,51 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import com.example.heechintong.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val registertext: TextView = findViewById(R.id.textViewLoginRegister)
+        auth = Firebase.auth
 
-        registertext.setOnClickListener{
+        binding.textViewLoginRegister.setOnClickListener{
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.buttonSubmitLogin.setOnClickListener {
+            performLogin()
+        }
+    }
+
+    private fun performLogin() {
+        val email = binding.editTextLoginEmail.text.toString()
+        val password = binding.editTextLoginUserPass.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this, "Email and password cannot be blank", Toast.LENGTH_SHORT).show()
+        }else{
+            auth.signInWithEmailAndPassword(email,password)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, DashBoardActivity::class.java)
+                    startActivity(intent)
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Error: " + it.message, Toast.LENGTH_SHORT).show()
+                }
         }
     }
 
--- app/src/main/java/com/example/heechintong/RegisterActivity.kt
+++ app/src/main/java/com/example/heechintong/RegisterActivity.kt
@@ -5,9 +5,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.example.heechintong.databinding.ActivityRegisterBinding
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -17,10 +19,12 @@ import com.google.firebase.ktx.Firebase
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityRegisterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         auth = Firebase.auth
 
@@ -29,35 +33,56 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        val loginBtn: Button = findViewById(R.id.buttonSubmitLogin)
+        loginBtn.setOnClickListener {
+            performRegister()
+        }
 // get email and password from user
-        performRegister()
+        // performRegister()
     }
 
     private fun performRegister() {
 
-        val email = findViewById<EditText>(R.id.editTextRegisterEmail)
-        val password = findViewById<EditText>(R.id.editTextRegisterUserPass)
+        val email = binding.editTextRegisterEmail
+        val password = binding.editTextPassword
+
 
         val inputEmail = email.text.toString()
         val inputPassword = password.text.toString()
 
-        auth.createUserWithEmailAndPassword(inputEmail, inputPassword)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "createUserWithEmail:success")
-                    val user = auth.currentUser
-                   //updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
-                    //updateUI(null)
+        if (inputEmail.isEmpty() || inputPassword.isEmpty()) {
+            Toast.makeText(
+                this, "Email or Password cannot be empty",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            Toast.makeText(this, "Hello here", Toast.LENGTH_SHORT).show()
+            auth.createUserWithEmailAndPassword(inputEmail, inputPassword)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        Toast.makeText(this, "Success!!!", Toast.LENGTH_SHORT).show()
+                        val user = auth.currentUser
+                        //updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        //updateUI(null)
+                    }
                 }
-            }
-
+                .addOnFailureListener {
+                    Toast.makeText(this, "Error: " + it.message, Toast.LENGTH_SHORT).show()
                 }
+        }
 
-            }
+
+    }
+
+}
 
